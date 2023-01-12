87fcb50dc287819682e415e08d83eb6ebe84c853
ParvizNajimov <101324794+ParvizNajimov@users.noreply.github.com>
2023-01-13 01:26:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const mainForm = document.forms.secions
+const mainFormInput = mainForm.nameInput
+
+mainForm.addeventListener('submit', function (event) {
+    if (required(mainFormInput)) {
+       mainFormInput.perentelement.insert
+    }
+})
